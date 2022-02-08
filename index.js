eaa802f0a812d59d12aed2f624a1ccf7652579a1
Carson <carsoncolgateccu@gmail.com>
2022-02-08 14:45:32 -0700

    added all my views except budget need to add an error to connection?

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const db = mysql.createConnection(
 // run inquirer
 
 pomptUser()
-function promptUser(){
+function promptUser() {
     return inquirer.prompt([
         {
             type: "list",
@@ -28,43 +28,64 @@ function promptUser(){
         }
     ])
 
-    .then((options) => {
-        if (options.displayoptions === "SHOW All Departments"){
-            viewDepartments();
-        }
-        if (options.displayoptions === "SHOW All Roles"){
-            viewRoles();
-        }
-        if (options.displayoptions === "SHOW All Employees"){
-            viewEmployees();
-        }
-        if (options.displayoptions === "SHOW Employees by Department"){
-            viewEmployeesByDepartment();
-        }
-        if (options.displayoptions === "Add Department"){
-            addDepartment();
-        }
-        if (options.displayoptions === "Add Role"){
-            addRole();
-        }
-        if (options.displayoptions === "Add Employee"){
-            addEmployee();
-        }
-        if (options.displayoptions === "Update Employee Role"){
-            updateEmployee();
-        }
-        if (options.displayoptions === "Delete Department"){
-            deleteDepartment();
-        }
-        if (options.displayoptions === "Delete Role"){
-            deleteRole();
-        }
-        if (options.displayoptions === "Delete Employee"){
-            deleteEmployee();
-        }
-        if (options.displayoptions === "View Department Budget"){
-            viewBudget();
-        }
+        .then((options) => {
+            if (options.displayoptions === "SHOW All Departments") {
+                viewDepartments();
+            }
+            if (options.displayoptions === "SHOW All Roles") {
+                viewRoles();
+            }
+            if (options.displayoptions === "SHOW All Employees") {
+                viewEmployees();
+            }
+            if (options.displayoptions === "SHOW Employees by Department") {
+                viewEmployeesByDepartment();
+            }
+            if (options.displayoptions === "Add Department") {
+                addDepartment();
+            }
+            if (options.displayoptions === "Add Role") {
+                addRole();
+            }
+            if (options.displayoptions === "Add Employee") {
+                addEmployee();
+            }
+            if (options.displayoptions === "Update Employee Role") {
+                updateEmployee();
+            }
+            if (options.displayoptions === "Delete Department") {
+                deleteDepartment();
+            }
+            if (options.displayoptions === "Delete Role") {
+                deleteRole();
+            }
+            if (options.displayoptions === "Delete Employee") {
+                deleteEmployee();
+            }
+            if (options.displayoptions === "View Department Budget") {
+                viewBudget();
+            }
 
-    })
+        })
 };
+// -------------------------VIEWS--------------------------------
+
+function viewDepartments() {
+    db.query('SELECT * FROM company_db.department;', function (err, results) {
+        console.table(results)
+        promptUser()
+    })
+}
+function viewRoles() {
+    db.query('select role.title, role.id, role.salary, department.name from department RIGHT JOIN role on department.id = role.department_id;', function (err, results) {
+        console.table(results)
+        promptUser()
+    })
+}
+function viewEmployees(){
+    db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id', function (err, results){
+        console.table(results)
+        promptUser()
+    })
+}
+
