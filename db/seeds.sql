f09daae5b7e0dbba984608487c4569fc993b2d08
Carson <carsoncolgateccu@gmail.com>
2022-02-08 13:33:56 -0700

    started working on my seeds added department and role/salary

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,18 @@
 USE company_db
+
+INSERT INTO department (name)
+VALUES ("Sales") --1
+        ("Engineering") --2
+        ("Finance") --3
+        ("Legal") --4
+
+INSERT INTO role ( title, salry, department_id)
+VALUES ("Sales Lead", 50000, 1)
+        ("Salesperson", 35000, 1)
+        ("Lead Engineer", 200000, 2)
+        ("Software Engineer", 125000, 2)
+        ("Account Manager", 10000, 3)
+        ("Accountant", 75000, 3)
+        ("Legal Team Lead", 150000, 4)
+        ("Lawyer", 175000, 4)
+
