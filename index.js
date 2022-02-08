d9b39a19043fa7272832b2094f8b071440a8bb0a
Carson <carsoncolgateccu@gmail.com>
2022-02-08 14:05:01 -0700

    started working on my inquirer to prompt my user of all their choices

--- index.js
+++ index.js
@@ -3,3 +3,32 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 require('dotenv').config()
 
+// create connection
+
+const db = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log("connected to db")
+);
+
+// run inquirer
+
+pomptUser()
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "displayoptions",
+            message: "Choose An Option",
+            choices: ["SHOW All Departments", "SHOW All Roles", "SHOW All Employees", "SHOW Employees by Department", "Add Department", "Add Role", "Add Employee", "Update Employee Role", "Delete Department", "Delete Role", "Delete Employee", "View Department Budget"]
+        }
+    ])
+
+    .then((options) => {
+        if
+    })
+}
